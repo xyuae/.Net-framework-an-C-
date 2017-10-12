fd69f7019e82ab3f1d8c15fab44f3a414d9f2284
Yu <x232yu@nexus.uwaterloo.ca>
2017-10-12 14:34:39 -0400

    Add project files.

---
+++ LearningDotNet/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LearningDotNet
+{
+    class Program
+    {
+        static ConcurrentDictionary<int, int> items = new ConcurrentDictionary<int, int>();
+
+        static void AddItem()
+        {
+            items.TryAdd(1, 2);
+            Console.WriteLine(items.Count);
+        }
+        static void Main(string[] args)
+        {
+
+            /* hashSet
+            var myHash = new HashSet<String>();
+
+            myHash.Add("hello");
+            myHash.Add("hello");
+
+            String[] s = new String[] { "hello" };
+            Console.WriteLine(myHash.Count);
+            Console.WriteLine(myHash.Overlaps(s));
+            /*
+            Queue<int> myQueue = new Queue<int>();
+
+            myQueue.Enqueue(1);
+            myQueue.Enqueue(2);
+            myQueue.Enqueue(3);
+
+            foreach (var number in myQueue)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine(myQueue.Dequeue());
+            Console.WriteLine(myQueue.Peek());
+            /*
+            Stack<String> pancakes = new Stack<String>();
+
+            pancakes.Push("first pancake made");
+            pancakes.Push("second pancake made");
+            pancakes.Push("Third pancake made");
+
+            foreach (var pancake in pancakes)
+            {
+                Console.WriteLine(pancake);
+            }
+
+            Console.WriteLine(pancakes.Pop());
+            Console.WriteLine(pancakes.Peek());
+            Console.WriteLine(pancakes.Peek());
+
+            /* Tuple
+            // Tuple<int, String, bool> myTuple = new Tuple<int, string, bool>(1, "hello", true);
+
+            var myTuple = Tuple.Create(1, "hello", true);
+
+            Console.WriteLine(myTuple.Item2);
+
+            /* Learning BitArray
+            bool[] preload = new bool[3] { true, false, true };
+            BitArray enemyGrid = new BitArray(preload);
+
+            
+            /* Learnign concurrency
+            Thread thread1 = new Thread(new ThreadStart(AddItem));
+            Thread thread2 = new Thread(new ThreadStart(AddItem));
+
+            thread1.Start();
+            thread2.Start();
+
+            /*
+            Console.WriteLine("Hello World");
+            List<String> customers = new List<string>();
+
+            customers.Add("Kim");
+            customers.Add("John");
+
+            Console.WriteLine(customers.Count);
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer);
+            }
+
+            Console.WriteLine(customers[1]);
+            
+
+            Dictionary<String, String> config = new Dictionary<string, string>();
+
+            config.Add("resolution", "1920x1080");
+            config.Add("title", "MyWebsite");
+            
+
+            Console.WriteLine(config["title"]);
+
+            foreach (var setting in config)
+            {
+                Console.WriteLine(setting.Value);
+            }
+
+            Hashtable table = new Hashtable();
+
+            table.Add("title", "MyWebsite");
+
+            var s = (String)table["title"];
+            Console.WriteLine(s);
+            */
+
+
+
+
+        }
+    }
+}
---
+++ LearningDotNet/study/readme.md
@@ -0,0 +1,7 @@
+﻿# Use Collection to Group Objects
+## Introduction to collections
+Another fundamental component of .Net is the collections library. Collectoins allow us to store data that can be grouped together because that is similiar. Collection also allow us to organie and sort data, as well as 
+to iterate or go through the dta, such as in a loop. A collection is held in memory and is just a data structure component of .NET, whereas a database is usually held on disk and is a standalone application.
+- Dictionaries for key-value pairs
+
+## Asynchronous Programming
