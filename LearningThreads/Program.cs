a2a885773ea0196e96e1b888b0e4dc8874e7aa1d
Yu <x232yu@nexus.uwaterloo.ca>
2017-10-16 14:16:04 -0400

    Thread

---
+++ LearningThreads/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LearningThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread mythread = new Thread(new ThreadStart(Work));
+            mythread.Start();
+
+            Console.ReadLine();
+        }
+
+        private static void Work()
+        {
+            //throw new NotImplementedException();
+            Thread.Sleep(3000);
+            Console.WriteLine("Thread complete. ")
+        }
+    }
+}
