65ba647c6dfb826b7806993c74eca805bc9a2db7
Yu <x232yu@nexus.uwaterloo.ca>
2017-10-17 11:09:01 -0400

    finish class

---
+++ LearningNull/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningNull
+{
+    class Person
+    {
+        public int MyProperty { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = null; //"hello";
+
+            int? i = null;
+            int i2 = i ?? 2;
+            var s2 = s ?? "default";
+
+            Console.WriteLine(s2);
+
+            try
+            {
+                throw new Exception();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            } 
+        }
+    }
+}
